2a43637207e461b2ba5d2f258839d4aea8e9a5dc
mushroomvoodoo <normeeboy@gmail.com>
2023-05-18 20:55:28 +0300

    Добавьте файлы проекта.

---
+++ Graphiki/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Graphiki
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(600, 600);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(762, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 60);
+            button1.TabIndex = 1;
+            button1.Text = "x^2";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(762, 87);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 60);
+            button2.TabIndex = 2;
+            button2.Text = "k/x";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(762, 153);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 60);
+            button3.TabIndex = 3;
+            button3.Text = "sin x";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(762, 219);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 60);
+            button4.TabIndex = 4;
+            button4.Text = "x^3";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(888, 120);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 60);
+            button5.TabIndex = 5;
+            button5.Text = "ОЧИСТИТЬ";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(259, 615);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 17);
+            label1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1115, 641);
+            Controls.Add(label1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label1;
+    }
+}
---
+++ Graphiki/Form1.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Graphiki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+        int y, x;
+        int[] xs = new int[161];
+        int[] ys = new int[161];
+        Pen negro = new Pen(Color.Red, 1);
+        void getCoordinats()
+        {
+            y = pictureBox1.Width / 2;
+            for (x = 0; x < pictureBox1.Width; x++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.Black);
+            }
+            x = pictureBox1.Height / 2;
+            for (y = 0; y < pictureBox1.Height; y++)
+            {
+                ((Bitmap)pictureBox1.Image).SetPixel(x, y, Color.Black);
+            }
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "Парабола";
+            getCoordinats();
+
+            x = -80;
+            for (int i = 0; i < xs.Length - 1; i++)
+            {
+                y = (x * x) / 13;
+                xs[i] = x;
+                ys[i] = y;
+                x++;
+            }
+            int h = 1;
+            for (int j = 1; j < ys.Length; j++)
+            {
+                using (var graphics = Graphics.FromImage(((Bitmap)pictureBox1.Image)))
+                {
+                    graphics.DrawLine(negro, 300 - xs[h], 300 - ys[j], 300 - xs[h - 1], 300 - ys[j - 1]);
+                }
+                h++;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Гипербола";
+            getCoordinats();
+            float k = 60;
+            int xMin = -(int)k;
+            int xMax = (int)k;
+            int yMax = (int)k;
+            int yMin = -(int)k;
+            int dx = pictureBox1.Width / (xMax - xMin);
+            int dy = pictureBox1.Height / (yMax - yMin);
+            float lastx = -100, lasty = -100;
+            for (float x = xMin; x < xMax; x = (float)(x + dx * 0.01))
+            {
+                float y = k / x;
+                float xg = dx * (x - xMin);
+                float yg = dy * (yMax - y);
+                using (var hyper = Graphics.FromImage(((Bitmap)pictureBox1.Image)))
+                {
+                    hyper.DrawLine(negro, xg, yg, lastx, lasty);
+                }
+                lastx = xg;
+                lasty = yg;
+            }
+            getCoordinats();
+            pictureBox1.Refresh();
+        }
+        float[] sinxs = new float[600];
+        float[] sinys = new float[600];
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Синусоида";
+            getCoordinats();
+            float sinx = 0;
+            for (int i = 0; i < sinxs.Length; i++)
+            {
+                float siny = (float)Math.Sin(sinx) * 7;
+                sinxs[i] = sinx;
+                sinys[i] = siny;
+                sinx += 0.07f;
+            }
+            int k = 1;
+            for (int j = 1; j < sinxs.Length; j++)
+            {
+                using (var sinusida = Graphics.FromImage(((Bitmap)pictureBox1.Image)))
+                {
+                    sinusida.DrawLine(negro, sinxs[k] * 17, 300 - sinys[j] * 11, sinxs[k - 1] * 17, 300 - sinys[j - 1] * 11);
+                }
+                k++;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getCoordinats();
+            label1.Text = "Кубическая парабола";
+            x = -80;
+            for (int i = 0; i < xs.Length; i++)
+            {
+                y = (x * x * x) / 1700;
+                xs[i] = x;
+                ys[i] = y;
+                x++;
+            }
+            int h = 1;
+            for (int j = 1; j < ys.Length; j++)
+            {
+                using (var graphics = Graphics.FromImage(((Bitmap)pictureBox1.Image)))
+                {
+                    graphics.DrawLine(negro, 300 - xs[h], 300 - ys[j], 300 - xs[h - 1], 300 - ys[j - 1]);
+                }
+                h++;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            getCoordinats();
+            label1.Text = null;
+        }
+    }
+}
